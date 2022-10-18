ed2dc6ba07b87b9c75cd218df87cc4425f1cce25
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-10-18 15:56:45 +0600

    navbar style add

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-const currency_check = document.querySelector(".currency");
+// const currency_check = document.querySelector(".currency");
 
-currency_check.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log("Clicked");
-    document.querySelector(".pop-up").classList.toggle("pop-up-show");
-})
+// currency_check.addEventListener('click', function(e){
+//     e.preventDefault();
+//     console.log("Clicked");
+//     document.querySelector(".pop-up").classList.toggle("pop-up-show");
+// })

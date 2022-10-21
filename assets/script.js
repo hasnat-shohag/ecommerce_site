053e09940a396742cade484c47951f93334364de
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-10-21 21:24:55 +0600

    completed the cart, sidebar

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,10 @@ document.querySelector('.sidebar_close').addEventListener('click', function(e){
     // console.log("Clicked");
     document.querySelector(".sidebar").classList.toggle("sidebar_show");
 })
+
+const cart = document.querySelector('.cart');
+cart.addEventListener('click', function(e){
+    e.preventDefault();
+    console.log("Clicked");
+    document.querySelector(".cart_content").classList.toggle("cart_content_show");
+})

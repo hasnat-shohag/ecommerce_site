f7bf85f99ffc38896363dcabd87268ea918ff913
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2022-10-30 12:12:52 +0600

    bottom nav complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,13 @@ document.querySelector('.sidebar_close').addEventListener('click', function(e){
 const cart = document.querySelector('.cart');
 cart.addEventListener('click', function(e){
     e.preventDefault();
-    console.log("Clicked");
+    // console.log("Clicked");
     document.querySelector(".cart_content").classList.toggle("cart_content_show");
+})
+
+const category_selector = document.querySelector(".all_category_trigger");
+category_selector.addEventListener('click', (e)=>{
+    e.preventDefault();
+    // console.log("clicked category");
+    document.querySelector('.error_handle').classList.toggle("show_all_category_list");
 })
